9ad94b77987b84db6043cbec6a020833db965b27
haingo <haingo@trudi.ai>
2024-07-01 14:53:03 +0700

    create sidebar

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {AdminComponent} from "@app/modules/admin/admin.component";
+import {MangaManagementComponent} from "@app/modules/admin/modules/manga-management/manga-management.component";
+import {UserManagementComponent} from "@app/modules/admin/modules/user-management/user-management.component";
+import {MyProfileComponent} from "@app/modules/admin/modules/my-profile/my-profile.component";
 
 const routes: Routes = [{
   path: '',
-  component: AdminComponent
+  component: AdminComponent,
+  children: [{
+    path: 'manga',
+    component: MangaManagementComponent
+  }, {
+    path: 'user',
+    component: UserManagementComponent
+  }, {
+    path: 'my-profile',
+    component: MyProfileComponent
+  }]
 }]
 
 @NgModule({
