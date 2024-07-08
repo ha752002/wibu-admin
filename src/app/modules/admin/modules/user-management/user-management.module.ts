4ba82875ff4d1f73b4118b94465f7919a05dee10
nguyenvanhoang0 <hoang03759@gmail.com>
2024-07-08 14:58:59 +0700

    fix app module

--- src/app/modules/admin/modules/user-management/user-management.module.ts
+++ src/app/modules/admin/modules/user-management/user-management.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UserManagementComponent} from "@app/modules/admin/modules/user-management/user-management.component";
+import {UserManagementRoutingModule} from "./user-management-routing.module";
 
 
 @NgModule({
   declarations: [UserManagementComponent],
   imports: [
+    UserManagementRoutingModule,
     CommonModule
   ]
 })
