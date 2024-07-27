db465ae415f755f826bf4a17f837cb2e634c8a55
honggha <honggha75@gmail.com>
2024-07-27 18:16:32 +0700

    delete code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import {EffectsModule} from '@ngrx/effects';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {StoreModule} from '@ngrx/store';
-import {IconDefinition} from '@ant-design/icons-angular';
-import {LockOutline, UserOutline} from '@ant-design/icons-angular/icons';
-import {NZ_ICONS} from 'ng-zorro-antd/icon';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {AuthEffect} from "@app/core/store/_auth/_auth.effects";
 import {combinedReducers} from "@app/core/store";
@@ -14,9 +11,6 @@ import {NzMessageModule} from "ng-zorro-antd/message";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {apiResolverInterceptor} from "@app/core/interceptors/api-resolver.interceptor";
 
-
-
-const icons: IconDefinition[] = [UserOutline, LockOutline];
 
 @NgModule({
   declarations: [AppComponent],
@@ -30,7 +24,6 @@ const icons: IconDefinition[] = [UserOutline, LockOutline];
     EffectsModule.forRoot([AuthEffect])
   ],
   providers: [
-    {provide: NZ_ICONS, useValue: icons},
     provideHttpClient(
       withInterceptors([apiResolverInterceptor]),
     )
